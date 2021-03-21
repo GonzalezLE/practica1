1bf5fd586dba8e0711be13e57fe387a7cea0a18d
Luis Enrique Gonzalez Arellano <37777541+GonzalezLE@users.noreply.github.com>
2021-03-20 23:25:55 -0600

    aprendiendo vista genericas

--- aplications/persona/urls.py
+++ aplications/persona/urls.py
@@ -3,9 +3,27 @@ from django.urls import path
 
 from . import views
 
+app_name='personal_app'
+
 urlpatterns = [
   path('listar-todo-empleados/', views.listAllEmpleados.as_view()),
   path('listar-by-area/<shorname>/', views.listByAreaEmpleado.as_view()),
   path('buscar-empleado/', views.ListEmpleadosByKwords.as_view()),
   path('lista-habilidades/<id>', views.ListaHabilidadesEmpleado.as_view()),
+  path('ver-empleado/<pk>/', views.EmpleadosDetailView.as_view()),
+  path('add-empleado/', views.EmpleadopCreateView.as_view()),
+  path(
+    'success/', 
+    views.SuccessView.as_view(), 
+    name='correcto'
+  ),
+  path(
+    'update-empleado/<pk>/', 
+    views.EmpleadoUpdateView.as_view(),
+    name='modificar_empleado'),
+  path(
+    'delete-empleado/<pk>/',
+    views.EmpleadoDeleteView.as_view(),
+    name='eliminar_empleado'
+  )
 ]
--- aplications/persona/views.py
+++ aplications/persona/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
 # Create your views here.
 from django.views.generic import (
-  ListView
+  ListView,
+  DetailView,
+  CreateView,
+  TemplateView,
+  UpdateView,
+  DeleteView
  )
 
 from .models import Persona
@@ -44,5 +49,69 @@ class ListaHabilidadesEmpleado(ListView):
     id_empleado=self.kwargs['id']
     empleado=Persona.objects.get(id=id_empleado)
     return  empleado.habilidades.all()
+
+
+class EmpleadosDetailView(DetailView):
+  model = Persona
+  template_name = "empleado/detail_empleado.html"
+  
+  def get_context_data(self, **kwargs):
+    context = super(EmpleadosDetailView, self).get_context_data(**kwargs)
+    context['title'] = 'Empleado del mes'
+    return context
   
 
+
+class SuccessView(TemplateView):
+  template_name ='empleado/success.html'
+
+class EmpleadopCreateView(CreateView):
+  """crea formulario"""
+  model = Persona
+  template_name = "empleado/add.html"
+  fields = [
+    'first_name', 
+    'last_name', 
+    'job', 
+    'departamento', 
+    'habilidades'
+    ]
+  #fields = ('__all__') 
+  success_url = reverse_lazy('personal_app:correcto')
+  def form_valid(self, form):
+    #logica del proceso
+    empleado=form.save(commit=False)#aqui ya se guardo
+    empleado.full_name = empleado.first_name + ' ' + empleado.last_name
+    empleado.save()
+    return super(EmpleadopCreateView,self).form_valid(form)
+
+
+
+class EmpleadoUpdateView(UpdateView):  
+  template_name = "empleado/update.html"
+  model = Persona
+  fields = [
+    'first_name',
+    'last_name',
+    'job',
+    'departamento',
+    'habilidades'
+  ]
+  success_url = reverse_lazy('personal_app:correcto')
+
+  def post(self, request, *args, **kwargs):
+    self.object=self.get_object()
+    print('************** Metodos POST *********************')    
+    print(request.POST['last_name'])
+    return super().post(request, *args, **kwargs)
+  
+  def form_valid(self, form):
+    print('************** Metodos form  *********************')
+    print('***********************************')
+    return super(EmpleadoUpdateView, self).form_valid(form)
+
+
+class EmpleadoDeleteView(DeleteView):
+  model = Persona
+  template_name = "empleado/delete.html"
+  success_url = reverse_lazy('personal_app:correcto')
